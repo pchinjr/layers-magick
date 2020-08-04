d7e3ad26706c7e3a02372d645bf99b4f2cf6c1d0
Paul Chin Jr <pchinjr@gmail.com>
2020-08-04 15:50:07 -0400

    os.homedir()

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -1,9 +1,11 @@
 let arc = require('@architect/functions')
 const { execSync } = require("child_process");
-
+const os = require('os')
 exports.handler = async function http(request) {
   
-  execSync('which gm', { encoding: 'utf8', stdio: 'inherit' })
+  console.log(os.homedir())
+
+  //execSync( 'gm version', { encoding: 'utf8', stdio: 'inherit' })
   
   return {
     headers: {'content-type': 'text/html; charset=utf-8;'},

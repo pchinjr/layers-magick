85256964aebfc2e7e9849a87d0b0a16e94816740
Paul Chin Jr <pchinjr@gmail.com>
2020-08-04 15:36:34 -0400

    two layers

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -2,6 +2,9 @@ let arc = require('@architect/functions')
 const { execSync } = require("child_process");
 
 exports.handler = async function http(request) {
+  
+  execSync('gm version', { encoding: 'utf8', stdio: 'inherit' })
+  
   return {
     headers: {'content-type': 'text/html; charset=utf-8;'},
     body: '<h1>Hello World! 🎉</h1>'

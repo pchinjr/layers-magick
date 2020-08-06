5a8b8a56a007e6a25e402de521fa9d6b3b12aa22
Paul Chin Jr <pchinjr@gmail.com>
2020-08-06 11:15:50 -0400

    resize existing file

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -1,16 +1,19 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 const { execSync } = require('child_process')
+const fs = require('fs')
 
 exports.handler = async function http (req) {
 
-  await execSync('gm version', { encoding: 'utf8', stdio: 'inherit' })
-  await execSync('gm convert -size 640x480 xc:red canvas.png', { encoding: 'utf8', stdio: 'inherit' })
+  await execSync('gm convert -size 120x120 me_sloth.jpg -resize 120x120 +profile "*" /tmp/thumbnail.png', { encoding: 'utf8', stdio: 'inherit' })
+
+  let image = fs.readFileSync('/tmp/thumbnail.png')
 
   return {
     headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
+      "statusCode": 200,
+      "Content-Type": 'image/png'
     },
-    body: `Praise Cage`
+    body: image.toString('base64'),
+    isBase64Encoded: true
   }
 }

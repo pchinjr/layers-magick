2771055d23a489d6ee2db853791cd29c293359c3
Paul Chin Jr <pchinjr@gmail.com>
2020-08-06 10:13:14 -0400

    gm convert red box

--- src/http/post-upload/index.js
+++ src/http/post-upload/index.js
@@ -1,7 +1,11 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
-
+const { execSync } = require('child_process')
 
 exports.handler = async function http (req) {
+
+  await execSync('gm version', { encoding: 'utf8', stdio: 'inherit' })
+  await execSync('gm convert -size 640x480 xc:red canvas.png', { encoding: 'utf8', stdio: 'inherit' })
+
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
